8ecd10d325db0671a823bf574dfb99045be28e95
wangxi <xib1102@icloud.com>
2022-04-06 16:49:04 +0800

    add backtesting tool

--- ru_backtesting.py
+++ ru_backtesting.py
@@ -3,35 +3,44 @@ from datetime import datetime
 from abmacd.abmacd_v2 import ABMACDStrategy
 from backtesting_tool import run_backtesting, show_portafolio
 
-
-
+one_year = {"start": datetime(2021, 2, 16),
+            "end": datetime(2022, 2, 16), "period": 1}
+ten_year = {"start": datetime(2012, 2, 16), "end": datetime(
+    2022, 2, 16), "period": 10}
 
 tasks = ['1d4h', '1d1h', '1h15min']
-
 logs = []
 
-for task in tasks:
-    engine = run_backtesting(
-    strategy_class=ABMACDStrategy, 
-    setting={'size':10, 'macd_lvl':task, 'sm_debug':False}, 
-    vt_symbol="RU88.SHFE",
-    interval="1m", 
-    start=datetime(2021, 2, 16), 
-    end=datetime(2022, 2, 16),
-    rate=0.45 / 10000,
-    slippage=5,
-    size=10,
-    pricetick=5,
-    capital=5_00_000
-    )
-    df = engine.calculate_result()
-    statistics = engine.calculate_statistics(output=False)
-    log = f"{task}\t 年化收益：\t {statistics['annual_return']:,.2f}%\t 百分比最大回撤：\t {statistics['max_ddpercent']:,.2f}%\t 夏普比率：\t {statistics['sharpe_ratio']:,.2f}"
 
-    logs.append(log)
-    
+def run(period: int):
+    tm = one_year
+    if period == 10:
+        tm = ten_year
+
+    for task in tasks:
+        engine = run_backtesting(
+            strategy_class=ABMACDStrategy,
+            setting={'size': 10, 'macd_lvl': task, 'sm_debug': False},
+            vt_symbol="RU88.SHFE",
+            interval="1m",
+            start=tm["start"],
+            end=tm["end"],
+            rate=0.45 / 10000,
+            slippage=5,
+            size=10,
+            pricetick=5,
+            capital=5_00_000
+        )
+        df = engine.calculate_result()
+        statistics = engine.calculate_statistics(output=False)
+        log = f"周期：{tm['period']}年\t{task}\t 年化收益：\t {statistics['annual_return']:,.2f}%\t 百分比最大回撤：\t {statistics['max_ddpercent']:,.2f}%\t 夏普比率：\t {statistics['sharpe_ratio']:,.2f}"
+
+        logs.append(log)
+
+
+run(1)
+run(10)
+
 print("Strategy: ABMACD")
-print("周期： 1年")
 for log in logs:
     print(log)
-
