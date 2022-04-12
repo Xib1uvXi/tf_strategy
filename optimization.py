b9eb5816cfb92d55eeb2a49d0ad73d68b186f44c
wangxi <xib1102@icloud.com>
2022-04-12 14:07:35 +0800

    add blvlma

--- optimization.py
+++ optimization.py
@@ -3,16 +3,18 @@ from vnpy.trader.optimize import OptimizationSetting
 
 
 def run_opt_b_ma_window_annual_retrun():
-    opt = new_default_optimizer()
-    opt_setting = OptimizationSetting()
-    opt_setting.set_target('annual_return')
-    opt_setting.params['macd_lvl'] = ['1h15min']
-    opt_setting.add_parameter('b_ma_window', 60, 66, 1)
+    lvls = ['1h15min', '1d1h', '1d4h']
 
-    opt.set_optimization_setting(opt_setting, opt_target_filter_by_annual_return)
-    opt.set_cg_setting(10, cg_target_filter_by_annual_return)
+    for lvl in lvls:
+        opt = new_default_optimizer()
+        opt_setting = OptimizationSetting()
+        opt_setting.set_target('annual_return')
+        opt_setting.params['macd_lvl'] = [lvl]
+        opt_setting.add_parameter('b_ma_window', 3, 5, 1)        
+        opt.set_optimization_setting(opt_setting, opt_target_filter_by_annual_return)
+        opt.set_cg_setting(10, cg_target_filter_by_annual_return)
+        opt.run_opt()
 
-    opt.run_opt()
 
 if __name__ == '__main__':
     run_opt_b_ma_window_annual_retrun()
