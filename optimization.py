33632c02c8ecb6f93c7a33cea3b473b1e8300ea1
wangxi <xib1102@icloud.com>
2022-04-12 15:00:45 +0800

    add bma

--- optimization.py
+++ optimization.py
@@ -10,7 +10,7 @@ def run_opt_b_ma_window_annual_retrun():
         opt_setting = OptimizationSetting()
         opt_setting.set_target('annual_return')
         opt_setting.params['macd_lvl'] = [lvl]
-        opt_setting.add_parameter('b_ma_window', 3, 5, 1)        
+        opt_setting.add_parameter('b_ma_window', 3, 66, 1)        
         opt.set_optimization_setting(opt_setting, opt_target_filter_by_annual_return)
         opt.set_cg_setting(10, cg_target_filter_by_annual_return)
         opt.run_opt()
