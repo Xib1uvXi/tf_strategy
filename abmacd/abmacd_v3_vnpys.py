10f40b64a19363a00a216c29273fb8859342f06d
wangxi <xib1102@icloud.com>
2022-04-24 19:48:10 +0800

    v3 vnpy add stoploss

--- abmacd/abmacd_v3_vnpys.py
+++ abmacd/abmacd_v3_vnpys.py
@@ -11,15 +11,18 @@ from vnpy_ctastrategy import (
 from vnpy.trader.constant import Interval, Direction, Offset, Status
 
 from abmacd.abmacd_v3 import ABMACDStrategy, ABMACDStrategyConfig
+from abmacd.strategy_model.riskctl_model.stop_loss import Stoploss
 from abmacd.strategy_model.signal_model.macd_sm import ABMacdAction
 
 from abmacd.ft_bargenerator import BarGenerator
+
 
 class ABMACDStrategyByVN(CtaTemplate):
     """"""
     author = "Xib"
 
     mswap_enable = False
+    stoploss_enable = False
 
     b_ma_window = 10
 
@@ -47,10 +50,9 @@ class ABMACDStrategyByVN(CtaTemplate):
     last_bar = None
     target_pos: float = 0
 
-
     parameters = ["a_fast_window", "a_slow_window",
                   "a_signal_period", "b_fast_window", "b_slow_window",
-                  "b_signal_period", "size", "macd_lvl",  "b_ma_window", "mswap_enable"]
+                  "b_signal_period", "size", "macd_lvl",  "b_ma_window", "mswap_enable", "stoploss_enable"]
 
     variables = ["a_fast_macd0", "a_fast_macd1", "a_slow_macd0", "a_slow_macd1",
                  "b_fast_macd0", "b_fast_macd1", "b_slow_macd0", "b_slow_macd1", "size", "macd_lvl"]
@@ -62,20 +64,16 @@ class ABMACDStrategyByVN(CtaTemplate):
         self.write_log(strategy_name)
         self.write_log(setting)
 
-        config = ABMACDStrategyConfig()
-        config.a_fast_window = self.a_fast_window
-        config.a_slow_window = self.a_slow_window
-        config.a_signal_period = self.a_signal_period
-        config.b_fast_window = self.b_fast_window
-        config.b_slow_window = self.b_slow_window
-        config.b_signal_period = self.b_signal_period
-        config.b_ma_window = self.b_ma_window
-        config.macd_lvl = self.macd_lvl
-        config.mswap_enable = self.mswap_enable
+        config = ABMACDStrategyConfig(self.mswap_enable, self.b_ma_window, self.a_fast_window, self.a_slow_window,
+                                      self.a_signal_period, self.b_fast_window, self.b_slow_window, self.b_signal_period, self.macd_lvl)
 
         self.sm = ABMACDStrategy(config, self.mock_action_handler)
 
         self.cancel_bg = BarGenerator(self.on_bar, 15, self.on_15min_bar)
+
+        self.long_stoploss = Stoploss(1, 0.02)
+
+        self.short_stoploss = Stoploss(-1, 0.02)
 
     def on_15min_bar(self, bar: BarData):
         """
@@ -95,7 +93,7 @@ class ABMACDStrategyByVN(CtaTemplate):
             self._close_long(price, is_blvl=True)
 
         elif action is ABMacdAction.B_CLOSE_SHORT:
-            self._close_short(price)       
+            self._close_short(price)
 
         elif action is ABMacdAction.B_OPEN_LONG_A:
             self._open_long(price, is_back=True)
@@ -111,13 +109,13 @@ class ABMACDStrategyByVN(CtaTemplate):
 
         elif action is ABMacdAction.A_RB_SHORT:
             self._rollback_long_to_short(price)
-        
+
         elif action is ABMacdAction.A_CLOSE_SHORT:
             self._close_short(price)
-        
+
         elif action is ABMacdAction.MS_CLOSE_SHORT:
             self._close_short(price)
-        
+
         elif action is ABMacdAction.MS_CLOSE_LONG:
             self._close_long(price)
 
@@ -125,7 +123,7 @@ class ABMACDStrategyByVN(CtaTemplate):
             if self.pos != 0:
                 if self.pos > 0:
                     self.sell(self.last_bar.close_price, abs(self.pos))
-                
+
                 if self.pos < 0:
                     self.cover(self.last_bar.close_price, abs(self.pos))
 
@@ -165,6 +163,12 @@ class ABMACDStrategyByVN(CtaTemplate):
 
         self.cancel_bg.update_bar(bar)
 
+        if self.stoploss_enable:
+            if self.long_stoploss.need_close(bar.close_price):
+                self.sell(bar.close_price, abs(self.pos))
+            if self.short_stoploss.need_close(bar.close_price):
+                self.cover(bar.close_price, abs(self.pos))
+
         self.sm.on_bar(bar)
 
     def on_order(self, order: OrderData):
@@ -175,7 +179,7 @@ class ABMACDStrategyByVN(CtaTemplate):
             if order.offset == Offset.CLOSE:
                 if order.direction == Direction.LONG:
                     self.cover(self.last_bar.close_price, order.volume)
-                
+
                 if order.direction == Direction.SHORT:
                     self.sell(self.last_bar.close_price, order.volume)
 
@@ -183,8 +187,21 @@ class ABMACDStrategyByVN(CtaTemplate):
         """
         Callback of new trade data update.
         """
-        pass
+        if self.stoploss_enable:
+            self._update_stoploss_by_trade(trade)
 
+    def _update_stoploss_by_trade(self, trade: TradeData):
+        if trade.direction is Direction.LONG and trade.offset is Offset.OPEN:
+            self.long_stoploss.open(1, trade.price, trade.volume)
+
+        if trade.direction is Direction.SHORT and trade.offset is Offset.OPEN:
+            self.short_stoploss.open(-1, trade.price, trade.volume)
+
+        if trade.direction is Direction.LONG and trade.offset is Offset.CLOSE:
+            self.short_stoploss.close(-1)
+
+        if trade.direction is Direction.SHORT and trade.offset is Offset.CLOSE:
+            self.long_stoploss.close(1)
 
     def on_stop_order(self, stop_order: StopOrder):
         """
@@ -195,7 +212,7 @@ class ABMACDStrategyByVN(CtaTemplate):
     def _open_long(self, price: float, is_back: bool = False, is_blvl: bool = False):
         if self.pos < 0:
             return
-        
+
         if (not is_blvl) and abs(self.pos) != 0:
             return
 
@@ -204,7 +221,7 @@ class ABMACDStrategyByVN(CtaTemplate):
             self._reset_target_pos()
         else:
             self.buy(price, self.size)
-    
+
     def _open_short(self, price: float, is_back: bool = False):
         if abs(self.pos) != 0:
             return
@@ -213,7 +230,7 @@ class ABMACDStrategyByVN(CtaTemplate):
             self.short(price, self.size)
         else:
             self.short(price, self.size)
-    
+
     def _close_long(self, price: float, is_blvl: bool = False):
         if abs(self.pos) == 0:
             return
@@ -234,15 +251,15 @@ class ABMACDStrategyByVN(CtaTemplate):
     def _rollback_short_to_long(self, price: float):
         if self.pos > 0:
             return
-        
+
         if self.pos == 0:
             self.buy(price, self.size)
             return
-        
+
         if self.pos < 0:
             self.cover(price, abs(self.pos))
             self.buy(price, self.size)
-    
+
     def _rollback_long_to_short(self, price: float):
         if self.pos < 0:
             return
@@ -250,7 +267,7 @@ class ABMACDStrategyByVN(CtaTemplate):
         if self.pos == 0:
             self.short(price, self.size)
             return
-        
+
         if self.pos > 0:
             self.sell(price, abs(self.pos))
             self.short(price, self.size)
